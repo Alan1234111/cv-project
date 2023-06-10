4aec1e3c4922e262d45552a61b7fc5d57b4736b3
Alan Pason <alan.pason132@gmail.com>
2023-06-10 17:55:56 +0200

    Create: Header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-function App() {
-  return <div></div>;
+import React, {Component} from "react";
+import Header from "./components/Header";
+
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <Header />
+      </main>
+    );
+  }
 }
 
 export default App;
