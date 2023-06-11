522291024300575f303fe5ed5550e7cf64f67771
Alan Pason <alan.pason132@gmail.com>
2023-06-11 14:30:05 +0200

    Add Components to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, {Component} from "react";
 import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
+import Achievements from "./components/Achievements";
 
 class App extends Component {
   render() {
     return (
-      <main>
+      <main className="main">
         <Header />
+        <div className="personal-information">
+          <AboutMe />
+          <Achievements />
+        </div>
       </main>
     );
   }
