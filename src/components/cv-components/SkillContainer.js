328f76a0310159a7032c2347cd27f2a5efcb0f14
Alan Pasoń <alan.pason132@gmail.com>
2023-06-14 20:14:47 +0200

    Feat: you can add and delete skills

--- src/components/cv-components/SkillContainer.js
+++ src/components/cv-components/SkillContainer.js
@@ -1,4 +1,5 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import deleteIcon from "../img/delete.svg";
 
 class SkillContainer extends Component {
   constructor(props) {
@@ -10,6 +11,18 @@ class SkillContainer extends Component {
       <div className="skill">
         <p className="skill-name">{this.props.skill}</p>
         <img className="rating-img" src={this.props.rating} alt="" />
+        {this.props.isEditable && (
+          <button
+            onClick={this.props.handleDelete}
+            className="delete-btn-container"
+          >
+            <img
+              className="delete-icon"
+              src={deleteIcon}
+              alt="Delete"
+            />
+          </button>
+        )}
       </div>
     );
   }
--- src/components/cv-components/Skills.js
+++ src/components/cv-components/Skills.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Heading from "./Heading";
 import SkillContainer from "./SkillContainer";
 import rating1 from "../img/rating1.png";
@@ -8,11 +8,91 @@ import rating4 from "../img/rating4.png";
 import rating5 from "../img/rating5.png";
 
 class Skills extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      skills: [
+        {
+          id: 1,
+          skillName: "Skill 1",
+          rating: 1,
+        },
+      ],
+      isEditable: false,
+    };
+  }
+
+  handleEdit = () => {
+    this.setState({
+      isEditable: !this.state.isEditable,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const skillName = e.target.elements["skill"].value;
+    const rating = e.target.elements["rating"].value;
+
+    const newSkills = {
+      id: Date.now(),
+      skillName: skillName,
+      rating: rating,
+    };
+    const updatedSkills = [...this.state.skills, newSkills];
+
+    this.setState({
+      skills: updatedSkills,
+      isEditable: false,
+    });
+  };
+
+  handleDelete = (id) => {
+    const updatedList = this.state.skills.filter(
+      (item) => item.id !== id
+    );
+
+    this.setState({
+      skills: updatedList,
+    });
+  };
+
   render() {
+    const raitings = [rating1, rating2, rating3, rating4, rating5];
     return (
       <div className="skills">
-        <Heading title="Skills:" />
-        <SkillContainer skill="Skill 1" rating={rating1} />
+        <Heading title="Skills:" handleEdit={this.handleEdit} />
+        {this.state.isEditable && (
+          <form onSubmit={this.handleSubmit} className="form">
+            <label htmlFor="skill">Add Skill:</label>
+            <input id="skill" />
+
+            <label htmlFor="rating">Rating(1-5):</label>
+            <input
+              className="input-number"
+              id="rating"
+              type="number"
+              min="1"
+              max="5"
+            />
+
+            <button type="submit" className="btn">
+              Submit
+            </button>
+          </form>
+        )}
+
+        {this.state.skills.map((skill) => {
+          return (
+            <SkillContainer
+              skill={skill.skillName}
+              rating={raitings[skill.rating - 1]}
+              handleDelete={() => this.handleDelete(skill.id)}
+              isEditable={this.state.isEditable}
+            />
+          );
+        })}
       </div>
     );
   }
