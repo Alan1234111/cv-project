db47c7f3ce5cec8995aa2a0afbf9b4e9b6f6127d
Alan Pasoń <alan.pason132@gmail.com>
2023-06-14 20:14:08 +0200

    Feat: editable summary

--- src/components/cv-components/Summary.js
+++ src/components/cv-components/Summary.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Heading from "./Heading";
 import EditBtn from "./EditBtn";
 
@@ -6,7 +6,8 @@ class Summary extends Component {
   constructor() {
     super();
     this.state = {
-      summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      summary:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       isEditable: false,
     };
   }
@@ -27,7 +28,15 @@ class Summary extends Component {
     return (
       <div className="summary">
         <Heading title="Summary:" handleEdit={this.handleEdit} />
-        <section>{this.state.isEditable ? <textarea onChange={this.handleChange}>{this.state.summary}</textarea> : <p>{this.state.summary}</p>}</section>
+        <section>
+          {this.state.isEditable ? (
+            <textarea onChange={this.handleChange}>
+              {this.state.summary}
+            </textarea>
+          ) : (
+            <p>{this.state.summary}</p>
+          )}
+        </section>
       </div>
     );
   }
