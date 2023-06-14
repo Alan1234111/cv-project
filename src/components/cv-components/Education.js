4fd312dd9788508025f65612a1e8f840442419ec
Alan Pasoń <alan.pason132@gmail.com>
2023-06-14 20:15:36 +0200

    Feat: you can add and delete education

--- src/components/cv-components/Education.js
+++ src/components/cv-components/Education.js
@@ -1,15 +1,103 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Heading from "./Heading";
 import EducationContainer from "./EducationContainer";
 
 class Education extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      education: [
+        {
+          startingYear: "2010",
+          endingYear: "2020",
+          subtitle: "Bachelors in Something",
+          courseDone: "SOME GOOD COLEGE",
+        },
+      ],
+      isEditable: false,
+    };
+  }
+
+  handleEdit = () => {
+    this.setState({
+      isEditable: !this.state.isEditable,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const startingYear = e.target.elements["starting-year"].value;
+    const endingYear = e.target.elements["ending-year"].value;
+    const schoolName = e.target.elements["school-name"].value;
+    const courseDone = e.target.elements["course-done"].value;
+
+    const newEducation = {
+      id: Date.now(),
+      startingYear: startingYear,
+      endingYear: endingYear,
+      schoolName: schoolName,
+      courseDone: courseDone,
+    };
+    const updatedEducation = [...this.state.education, newEducation];
+
+    this.setState({
+      education: updatedEducation,
+      isEditable: false,
+    });
+  };
+
+  handleDelete = (id) => {
+    const updatedList = this.state.education.filter(
+      (item) => item.id !== id
+    );
+
+    this.setState({
+      education: updatedList,
+    });
+  };
+
   render() {
     return (
       <div className="education">
-        <Heading title="Education:" />
+        <Heading title="Education:" handleEdit={this.handleEdit} />
 
         <div className="education-container">
-          <EducationContainer duration="2010 - 2020" subtitle="Bachelors in Something" school="SOME GOOD COLLEGE" />
+          {this.state.isEditable && (
+            <form onSubmit={this.handleSubmit} className="form">
+              <label htmlFor="starting-year">Starting Year:</label>
+              <input id="starting-year" />
+
+              <label htmlFor="ending-year">Ending Year:</label>
+              <input id="ending-year" />
+
+              <label htmlFor="school-name">
+                College/School Name:
+              </label>
+              <input id="school-name" />
+
+              <label htmlFor="course-done">Course Done:</label>
+              <input id="course-done" />
+
+              <button className="btn" type="submit">
+                Submit
+              </button>
+            </form>
+          )}
+
+          {this.state.education.map((education) => {
+            return (
+              <EducationContainer
+                key={education.id}
+                duration={`${education.startingYear} - ${education.endingYear}`}
+                subtitle={education.schoolName}
+                school={education.courseDone}
+                isEditable={this.state.isEditable}
+                handleDelete={() => this.handleDelete(education.id)}
+              />
+            );
+          })}
         </div>
       </div>
     );
--- src/components/cv-components/EducationContainer.js
+++ src/components/cv-components/EducationContainer.js
@@ -1,4 +1,5 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import deleteIcon from "../img/delete.svg";
 
 class EducationContainer extends Component {
   constructor(props) {
@@ -13,6 +14,19 @@ class EducationContainer extends Component {
           <h3 className="school-subtitle">{this.props.subtitle}</h3>
           <h4 className="school-name">{this.props.school}</h4>
         </div>
+
+        {this.props.isEditable && (
+          <button
+            onClick={this.props.handleDelete}
+            className="delete-btn-container"
+          >
+            <img
+              className="delete-icon"
+              src={deleteIcon}
+              alt="Delete"
+            />
+          </button>
+        )}
       </div>
     );
   }
